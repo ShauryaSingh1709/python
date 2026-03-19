22d03073547a1364e1eba58f2f3046d67ccf76e1
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-03-19 23:00:19 +0530

    first commit

--- swap.py
+++ swap.py
@@ -3,13 +3,13 @@ num = int(input("Enter a number: "))
 n = num
 last = n % 10
 n //= 10
-alavya = 1
+shau = 1
 while n >= 10:
     n //= 10
-    alavya += 1
+    shau += 1
 
 first = n 
-middle = (num % (10 ** alavya)) // 10
-swapped = last * (10 ** alavya) + middle * 10 + first
+middle = (num % (10 ** shau)) // 10
+swapped = last * (10 ** shau) + middle * 10 + first
 
 print("result:", swapped)
