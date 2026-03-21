ca7819410925005d1ef60b5d1e99165899652e19
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-03-21 12:16:51 +0530

    Shaurya

--- copy.py
+++ copy.py
@@ -13,3 +13,27 @@ print("Value of b: ", b)
 a = 20
 print("Value of a: ", a)
 print("Value of b: ", b)
+
+l = [10,9.56,'Hello',[7j,True,3]]
+p = l
+
+p[1] = 9+7j
+
+print("Value of l: ", l)
+print("Value of p: ", p)
+
+#Modification done to with respect to the source variable or destination variable will affect the other variable as well because both variables are pointing to the same memory address.(only in case of multiple values)
+
+
+#Shallow copy will copy the main memory space of the source variable into the destination variable.
+#syntax: Destination variable = source variable.copy()
+
+sh = [10,9.56,'Hello',[7j,True,3]]
+ur = sh.copy()
+print(id(sh))
+print(id(ur))
+#Here both have different memory addresses.
+ur[1] = 9+7j
+print("Value of sh: ", sh)
+print("Value of ur: ", ur)
+#Now modification done only in ur not in sh because both have different memory addresses.
