997bd0a8fa366ee5e7d1e4194e3afac296d3296a
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-04-06 21:59:04 +0530

    SHAURYA

--- variable.py
+++ variable.py
@@ -48,7 +48,9 @@ def vowels_count(string):
     count = 0
     for char in string:
         if char in vowels:
+            print(char,end='')
             count += 1
     return count
 string = input("Enter a string: ")
-print("Number of vowels:", vowels_count(string))
+print("Extracted Vowels in the string:", end=' ')
+print("\nNumber of vowels:", vowels_count(string))
