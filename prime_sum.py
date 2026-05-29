4ace5daeaaac6a7d64d5a07740aa282e4f085542
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-05-29 10:48:07 +0530

    Shaurya

--- prime_sum.py
+++ prime_sum.py
@@ -3,6 +3,18 @@ n = int(input("Enter end: "))
 
 total = 0
 
+
+def is_prime(num):
+    if num < 2:
+        return False
+
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
+
+
 for num in range(m, n + 1):
     if is_prime(num):
         total += num
